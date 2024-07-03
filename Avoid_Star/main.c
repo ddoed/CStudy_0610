aa47546c2ff6c4d8417f985fdb06cb074ac4a2a2
ddoed <yimnabin20051022@gmail.com>
2024-07-03 17:46:18 +0900

    (20240703) 별 피하기 게임 완성

--- Avoid_Star/main.c
+++ Avoid_Star/main.c
@@ -2,12 +2,37 @@
 #include "Console.h" // 프로젝트 안에 있는 파일 찾아서 복사
 #include <stdbool.h>
 #include <time.h>
+#include <stdlib.h>
+#include <conio.h> // console input output
 
 #define MAX 20 //전처리기로 MAX 숫자에 값을 전부 10으로 치환하는 코드
+#define BG 15
+
+void StartMenu(void)
+{
+	while (true)
+	{
+		GotoXY(9, 9);
+		printf("별피하기 게임");
+		GotoXY(4, 20);
+		printf("키를 입력하여 시작하세요");
+
+		if (_kbhit()) //아무키나 입력하면 True 아니면 False
+			break;
+	}
+}
+void GameOver(score)
+{
+	int temp;
+	printf("플레이어가 사망했습니다.\n최종점수 : %d \n\n게임을 종료하려면 아무 숫자키를 입력후 엔터\n",score);
+	scanf_s("%d", &temp);
+}
 
 
 int main()
 {
+	SetTitle("별피하기");
+	SetColor(BG, 0);
 	srand(time(NULL)); // 핸덤함수의 seed값을 현재 시간에 따라서 변경
 	// 콘솔창에 게임의 맵의 크기를 만들어야 한다
 	SetConsoleSize(30, 30);
@@ -15,6 +40,10 @@ int main()
 
 	//플레이어의 총알 구현
 	//gotoXY printf
+
+	int score = 0;
+	bool playerAlive = true;
+	int heart = 10;
 
 	int x = 14, y = 28; //플레이어의 xy좌표
 	int bx = 0, by = 0; //총알의 xy좌표
@@ -28,6 +57,8 @@ int main()
 	int ey[MAX] = { 0 };
 	bool enemy[MAX] = { false };
 
+	Clear();
+	StartMenu();
 
 
 
@@ -35,18 +66,31 @@ int main()
 #if true //플레이어의 이동 조작
 	while (true)
 	{
+
 		Clear(); //이전 위치를 지우고 새로 그리기
+		GotoXY(0, 0);
+		SetColor(15, 3);
+		printf("Score %d ", score); // score 출력
+		score++;
+
+		GotoXY(0, 1);
+		SetColor(15, 3);
+		printf("체력 : %d",heart);
+
+		GotoXY(0, 2);
+		SetColor(15, 3);
+		printf("Q : 폭탄");
 
 		if (GetAsyncKeyState(VK_LEFT) & 8001) // 왼쪽키를 눌렀을때 아래코드 실행
 		{
-			if (x < 1) x = 1;
-			x--;
+			if (x < 2) x = 2;
+			x -= 2;
 		}
 
 		else if (GetAsyncKeyState(VK_RIGHT) & 8001) // 왼쪽키를 눌렀을때 아래코드 실행
 		{
 			if (x > 27) x = 27;
-			x++;
+			x += 2;
 		}
 
 		else if (GetAsyncKeyState(VK_DOWN) & 8001) // 왼쪽키를 눌렀을때 아래코드 실행
@@ -57,14 +101,24 @@ int main()
 
 		else if (GetAsyncKeyState(VK_UP) & 8001) // 왼쪽키를 눌렀을때 아래코드 실행
 		{
-			if (y < 1) y = 1;
+			if (y < 2) y = 2;
 			y--;
 		}
 
 #endif
+		if (playerAlive)
+		{
+			SetColor(BG, 12);
+			GotoXY(x, y);
+			printf("♥");
+		}
+		else
+		{
+			GotoXY(x, y);
+			printf("@");
+			break;
+		}
 
-		GotoXY(x, y);
-		printf("♥");
 
 #if true // 총알
 		if (GetAsyncKeyState(VK_SPACE) & 8001)
@@ -76,6 +130,8 @@ int main()
 				by = y - 1;
 				bullet = true;
 			}
+
+
 		}
 
 		if (bullet) // 총알이 생성되어 있을때만
@@ -94,7 +150,7 @@ int main()
 		if (!enemy)
 		{
 			srand(time(NULL)); // 핸덤함수의 seed값을 현재 시간에 따라서 변경
-			ex = (rand() % 14)*2;
+			ex = (rand() % 14) * 2;
 			ey = 0;
 			enemy = true;
 		}
@@ -109,28 +165,44 @@ int main()
 			{
 				enemy = false;
 			}
-				
+
 		}
 #endif
 		for (int i = 0; i < MAX; i++)
 		{
 			if (!enemy[i])
 			{
-				ex[i] = (rand() % 14)*2;
+				ex[i] = (rand() % 14) * 2;
 				ey[i] = 0;
 				enemy[i] = true;
 				break;
 			}
 		}
-		
+
 		for (int i = 0; i < MAX; i++)
 		{
-			if (enemy[i]) // 총알이 생성되어 있을때만
+			if (enemy[i])
 			{
 				ey[i]++;
 				GotoXY(ex[i], ey[i]);
-				SetColor(0, i);
+				SetColor(BG, i);
 				printf("★");
+
+				if (ex[i] == bx && ey[i] == by && bullet == true)
+				{
+					enemy[i] = false;
+					bullet = false;
+					score += 100;
+
+				}
+
+				if (x == ex[i] & y == ey[i])
+				{
+					score -= 100;
+					heart--;
+					if (heart==0)
+						playerAlive = false;
+				}
 
 				if (ey[i] > 28) //총알이 화면 가장 위로 넘어갔을 때 비활성화된다
 				{
@@ -138,16 +210,21 @@ int main()
 				}
 
 			}
-
-			if (ex[i] == bx & ey[i] == by)
-			{
-				enemy[i] = false;
-				bullet = false;
-			}
-			
 		}
 
-		
+		if (GetAsyncKeyState(0x51) & 8001)
+		{
+			for (int i = 0; i < 10; i++)
+			{
+				enemy[i] = false;
+			}
+		}
+
 		Sleep(100);
 	}
+
+	Clear();
+	GameOver(score);
+	
+	
 }
